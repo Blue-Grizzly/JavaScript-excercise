10c947385e90fd7a7db953f77f107cb2318aa721
Blue-Grizzly <123905990+Blue-Grizzly@users.noreply.github.com>
2023-01-31 10:50:26 +0100

    First edition

---
+++ app.js
@@ -0,0 +1,4 @@
+function sayHello(){
+        document.querySelector("h3").textContent = "Hello " + document.querySelector("input").value;
+}
+document.querySelector("button").addEventListener("click", sayHello);
